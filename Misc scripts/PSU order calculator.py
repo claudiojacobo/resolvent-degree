169f878d1ef34445ae9cb28e909f71becd4dfd1a
pablo-christofferson <christoffersonp@carleton.edu>
2025-06-26 14:07:45 -0500

    Fixed input of get_PSU_order

--- Misc scripts/PSU order calculator.py
+++ Misc scripts/PSU order calculator.py
@@ -5,9 +5,10 @@ def get_PSU_order(n,q):
     :param q: prime
     :return: order of PSU(n+1, q)
     """
+    n += -1 # correction to make the wikipedia formula work (as it's for the differently labeled )
     k = 1
     for i in range(1, n+1):
         k *= q ** (i + 1) - ((-1) ** (i + 1))
     return k * (q ** ((1/2) * n * (n+1))) * (1/(math.gcd(n+1, q+1)))
 
-print(get_PSU_order(2, 13))
+print(get_PSU_order(3, 4))
