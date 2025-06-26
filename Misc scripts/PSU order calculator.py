431482ceaadf04da161ff5de3a171035b8d6d852
pablo-christofferson <christoffersonp@carleton.edu>
2025-06-26 13:48:19 -0500

    Removed Dependencies on global variables (testing needed)

--- Misc scripts/PSU order calculator.py
+++ Misc scripts/PSU order calculator.py
@@ -7,7 +7,7 @@ def get_PSU_order(n,q):
     """
     k = 1
     for i in range(1, n+1):
-        k *= q ** (i + 1) - (-1) ** (i + 1)
+        k *= q ** (i + 1) - ((-1) ** (i + 1))
     return k * (q ** ((1/2) * n * (n+1))) * (1/(math.gcd(n+1, q+1)))
 
-print(get_PSU_order(2, 5))
+print(get_PSU_order(2, 13))
